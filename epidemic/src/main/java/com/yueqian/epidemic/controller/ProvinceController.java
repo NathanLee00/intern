e78bca04f1518ad7da0ff1753af1e5580f3c623b
shepard <812176315@qq.com>
2020-10-18 15:33:31 +0800

    real one

--- epidemic/src/main/java/com/yueqian/epidemic/controller/ProvinceController.java
+++ epidemic/src/main/java/com/yueqian/epidemic/controller/ProvinceController.java
@@ -1,11 +1,13 @@
 package com.yueqian.epidemic.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.yueqian.epidemic.beans.AjaxResponseInfo;
 import com.yueqian.epidemic.beans.ProvinceInfo;
 import com.yueqian.epidemic.service.ProvinceService;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -22,11 +24,17 @@ public class ProvinceController {
 
     @ResponseBody
     @GetMapping("/ajax/noDataList")
-    public List<ProvinceInfo> getNoDataInput(String date){
+    public AjaxResponseInfo<List<ProvinceInfo>> getNoDataInput(String date){
         logger.debug("日期："+date);
+        AjaxResponseInfo<List<ProvinceInfo>> responseInfo=new AjaxResponseInfo<>();
         List<ProvinceInfo> list=provinceService.getProvinceNoDate(date);
-        ObjectMapper om=new ObjectMapper();
-
-         return list;
+        if(!StringUtils.isEmpty(date)){
+            list=provinceService.getProvinceNoDate(date);
+            responseInfo.setData(list);
+        }else{
+           responseInfo.setCode(-1);
+           responseInfo.setMsg("日期参数不正确");
+        }
+         return responseInfo;
     }
 }
